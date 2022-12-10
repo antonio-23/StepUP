832c824b65cd2f1474bcf4d9ba97cad9de0f6f9f
Antoni Garczyński <antek.garczynski@gmail.com>
2022-12-10 23:58:36 +0100

    Dodanie logowania i rejestracji

--- app/src/main/java/com/example/stepup/LoginActivity.java
+++ app/src/main/java/com/example/stepup/LoginActivity.java
@@ -1,13 +1,75 @@
 package com.example.stepup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText email, password;
+    private Button btnLogin;
+    private TextView textRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.login);
+        textRegister = findViewById(R.id.text_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    private void login(){
+        String user = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+
+        if (user.isEmpty()) {
+            email.setError("E-mail nie może być pusty.");
+        }
+        if (pass.isEmpty()) {
+            password.setError("Hasło nie może być puste.");
+        } else {
+            mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Zalogowano pomyślnie.", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Nieprawidłowy login lub hasło. Spróbuj ponownie." , Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/stepup/MainActivity.java
+++ app/src/main/java/com/example/stepup/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.stepup;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.ui.AppBarConfiguration;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
 
     public DrawerLayout drawerLayout;
@@ -18,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -30,6 +38,17 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
     }
 
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
