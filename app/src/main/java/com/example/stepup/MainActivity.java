d5af54d23b65b4eea58a1f5c22555c7814eeabc7
Jakub Jan Ćwik <cwikkuba0@gmail.com>
2023-01-09 18:41:15 +0100

    Naprawa Licznika kroków

--- app/src/main/java/com/example/stepup/MainActivity.java
+++ app/src/main/java/com/example/stepup/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.stepup;
 
 import androidx.appcompat.app.AppCompatDelegate;
 
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -12,6 +11,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -27,15 +27,11 @@ public class MainActivity extends DrawerBaseActivity implements SensorEventListe
     private FirebaseAuth mAuth;
     private Button btnLogout;
     //kroki
+    private TextView textViewStepCounter, textViewStepDetector;
     private SensorManager sensorManager;
-    private Sensor stepCounterSensor;
-    private int stepCount;
-    private void Dodaj() {
-        // Jeśli tak, zwiększ licznik kroków o jeden
-        stepCount += stepCount;
-        TextView stepCountTextView = findViewById(R.id.tv_stepsTaken); // Pobierz element TextView z interfejsu użytkownika
-        stepCountTextView.setText(String.valueOf(stepCount)); // Ustaw tekst elementu TextView na aktualną liczbę kroków
-    }
+    private Sensor mStepCounter,mStepDetector;
+    private boolean isCounterSensorPresent, isDetectorSensorPresent;
+    int stepCount=0, stepDetector=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +41,31 @@ public class MainActivity extends DrawerBaseActivity implements SensorEventListe
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //textViewStepCounter = findViewById(R.id.tv_stepsTaken);
+        textViewStepDetector = findViewById(R.id.tv_stepsTaken);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
         mAuth = FirebaseAuth.getInstance();
         btnLogout = findViewById(R.id.btnLogout);
 
         //kroki
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+
+            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isCounterSensorPresent=true;
+        }else{
+            //  textViewStepCounter.setText("Sensor dont worke");
+            isCounterSensorPresent= false;
+        }
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null) {
+            mStepDetector = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+            isDetectorSensorPresent = true;
+        }
+        else{
+            isDetectorSensorPresent= false;
+        }
     }
 
     @Override
@@ -63,31 +78,41 @@ public class MainActivity extends DrawerBaseActivity implements SensorEventListe
     }
 
     //kroki
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        return Service.START_STICKY;
-    }
-
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        // Sprawdź, czy wartość akceleracji w osi z przekroczyła pewien próg
-        if (event.values[2] > 5.0) {
-            Dodaj();
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            sensorManager.registerListener(this,mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null){
+            sensorManager.registerListener(this,mStepDetector, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
 
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            sensorManager.unregisterListener(this, mStepCounter);
+        }
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) != null){
+            sensorManager.unregisterListener(this, mStepDetector);
+        }
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor == mStepCounter){
+            stepCount = (int) sensorEvent.values[0];
+            //  textViewStepCounter.setText(String.valueOf(stepCount));
+        }else if (sensorEvent.sensor == mStepDetector){
+            stepDetector = (int) (stepDetector+sensorEvent.values[0]);
+            textViewStepDetector.setText(String.valueOf(stepDetector));
+        }
+    }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Ta metoda jest wymagana przez interfejs SensorEventListener, ale nie jest nam potrzebna w tym przypadku
-    }
 
-    public int getStepCount() {
-        return stepCount;
     }
 
     public void logout(View view){
